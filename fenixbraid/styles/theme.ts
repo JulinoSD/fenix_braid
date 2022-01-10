cec88ef56b35f299ed7338f8a149cfc4e75041c4
JulinoSD <ddimojulino@gmail.com>
2022-01-10 00:49:02 -0300

    theme 1

--- fenixbraid/styles/theme.ts
+++ fenixbraid/styles/theme.ts
@@ -6,6 +6,46 @@ export const theme = {
         secondary: '#f52a10',
         buttonOn: 'gren',
         buttonOf: 'red',
+    },
+    dimension: {
+        button: {
+            width: '150px',
+            height: '50px'
+        },
+        screen: {
+            width: '100vw',
+            height: '100vh'
+        },
+        splashScreen: {
+            title: {
+                color: 'red',
+                width: '250px',
+                heght: '30px',
+                borderRadios: '5px',
+            }
+        }
+    },
+    price: {
+        titlePrice: {
+            textTransform: 'uppercase',
+            color: '#d89667'
+        },
+        product: {
+            title: {
+                textTransform: 'uppercase',
+                fontSize: 'light',
+            },
+            subTitle: {
+                textTransform: 'lowercase',
+                color: 'inherit',
+            },
+            value: {
+                display: 'flex',
+                justifyContent: 'space-between',
+                fontSize: 'light',
+            }
+        }
     }
+
 }
 export default theme
